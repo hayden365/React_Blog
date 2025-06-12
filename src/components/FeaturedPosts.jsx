b3e2cbe22211cdbbf00e80c4d910e8e528319894
hayden365 <ysmh100@gmail.com>
2025-06-12 23:35:42 +0900

    feat: featured posts component

--- src/components/FeaturedPosts.jsx
+++ src/components/FeaturedPosts.jsx
@@ -1,5 +1,12 @@
 const FeaturedPosts = () => {
-  return <div>FeaturedPosts</div>;
+  return (
+    <div className="mt-8 flex flex-col lg:flex-row gap-8">
+      {/* First */}
+      <div className="w-full lg:w-1/2 flex flex-col gap-4">{/* image */}</div>
+      {/* Others */}
+      <div className="w-full lg:w-1/2 flex flex-col gap-4"></div>
+    </div>
+  );
 };
 
 export default FeaturedPosts;
