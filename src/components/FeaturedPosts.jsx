2e381c19dd3c9b5436a12bf6315ecce2e3953231
hayden365 <ysmh100@gmail.com>
2025-06-13 23:37:31 +0900

    feat: features image

--- src/components/FeaturedPosts.jsx
+++ src/components/FeaturedPosts.jsx
@@ -1,8 +1,15 @@
+import Image from "./Image";
+
 const FeaturedPosts = () => {
   return (
     <div className="mt-8 flex flex-col lg:flex-row gap-8">
       {/* First */}
-      <div className="w-full lg:w-1/2 flex flex-col gap-4">{/* image */}</div>
+      <div className="w-full lg:w-1/2 flex flex-col gap-4">
+        {/* image */}
+        <Image src="featured1.jpeg" className="rounded-3xl object-cover" />
+        {/* details */}
+        {/* items */}
+      </div>
       {/* Others */}
       <div className="w-full lg:w-1/2 flex flex-col gap-4"></div>
     </div>
