758fb7ffbc9223efba217a01c444597050e13873
hayden365 <ysmh100@gmail.com>
2025-06-25 12:10:51 +0900

    fix: webhook user 빠지는 에러 -> db index 삭제

--- backend/controllers/webhook.controller.js
+++ backend/controllers/webhook.controller.js
@@ -23,17 +23,19 @@ export const clerkWebHook = async (req, res) => {
     });
   }
 
-  console.log(evt.data);
-
   if (evt.type === "user.created") {
-    const newUser = new User({
-      clerkUserId: evt.data.id,
-      username: evt.data.username || evt.data.email_addresses[0].email_address,
-      email: evt.data.email_addresses[0].email_address,
-      img: evt.data.profile_img_url,
-    });
+    const existing = await User.findOne({ clerkUserId: evt.data.id });
+    if (!existing) {
+      const newUser = new User({
+        clerkUserId: evt.data.id,
+        username:
+          evt.data.username || evt.data.email_addresses[0].email_address,
+        email: evt.data.email_addresses[0].email_address,
+        img: evt.data.profile_img_url,
+      });
 
-    await newUser.save();
+      await newUser.save();
+    }
   }
 
   if (evt.type === "user.deleted") {
